4c183aec40e466d279f381675f414c249c196210
somuravikiran <ravikiranreddy7676@gmail.com>
2026-04-10 19:11:57 +0530

    MSA-00001: DoctorAvailability is completed.

--- DoctorAvailability_API/src/main/java/com/Medical_Appointment/DoctorAvailability_API/dto/DoctorAvailabilityRequest.java
+++ DoctorAvailability_API/src/main/java/com/Medical_Appointment/DoctorAvailability_API/dto/DoctorAvailabilityRequest.java
@@ -3,7 +3,6 @@ package com.Medical_Appointment.DoctorAvailability_API.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
